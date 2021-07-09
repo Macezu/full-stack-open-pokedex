3a99e25c573ffd62f6e55a00deb8f6dfb295e224
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-07-10 00:20:32 +0300

    imporved e2e test

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -3,7 +3,10 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
     cy.visit('http://localhost:8080')
-    cy.contains('ivysaur')
+    cy.contains('ivysaur').click()
+    cy.contains(
+      'chlorophyll'
+    )
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
