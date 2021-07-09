b8d206856155cb91c4d6e565143a78ab4ed60bed
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-07-10 00:32:07 +0300

    fixed port number from test file

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -2,13 +2,14 @@
 /* eslint-disable no-undef */
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:8080')
-    cy.contains('ivysaur').click()
-    cy.contains(
-      'chlorophyll'
-    )
+    cy.visit('http://localhost:5000')
+    cy.contains('squirtle')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  })
+  it('pokemon page can be opened', function () {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
