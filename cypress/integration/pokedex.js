a78640f92893719471fa3e35b6cf4ac01f00809d
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-07-10 14:42:49 +0300

    updated e2e and package.json

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -9,6 +9,7 @@ describe('Pokedex', function () {
     )
   })
   it('pokemon page can be opened', function () {
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
   })
